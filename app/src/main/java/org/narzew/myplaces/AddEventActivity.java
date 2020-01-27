5fd0d07d0dd657b7ba2fe56d9019336795c69799
Nikodem Solarz <narzew@gmail.com>
2020-01-27 17:01:45 +0500

    Verification, but not working

--- app/src/main/java/org/narzew/myplaces/AddEventActivity.java
+++ app/src/main/java/org/narzew/myplaces/AddEventActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,6 +22,7 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
     EditText event_name, event_description, event_author, event_location;
     TextView event_timing;
     DBHelper db;
+    Boolean date_set = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,15 +58,65 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
         datePickerDialog.show();
     }
 
+    /*
+    Verify if event form is correct
+     */
+
+    public Boolean verifyEvent(View v){
+        Boolean ok = true;
+        if(event_name.getText().toString().length() < 3){
+            ok = false;
+            Toast.makeText(this, getResources().getString(R.string.verify_event_name), Toast.LENGTH_LONG);
+            event_name.setBackgroundColor(Color.RED);
+        } else {
+            event_name.setBackgroundColor(Color.WHITE);
+        }
+        if(event_author.getText().toString().length() < 3){
+            ok = false;
+            Toast.makeText(this, getResources().getString(R.string.verify_event_author), Toast.LENGTH_LONG);
+            event_author.setBackgroundColor(Color.RED);
+        } else {
+            event_author.setBackgroundColor(Color.WHITE);
+        }
+        if(event_location.getText().toString().length() < 3){
+            ok = false;
+            Toast.makeText(this, getResources().getString(R.string.verify_event_location), Toast.LENGTH_LONG);
+            event_location.setBackgroundColor(Color.RED);
+        } else {
+            event_location.setBackgroundColor(Color.WHITE);
+        }
+        if(event_description.getText().toString().length() < 10){
+            ok = false;
+            Toast.makeText(this, getResources().getString(R.string.verify_event_description), Toast.LENGTH_LONG);
+            event_description.setBackgroundColor(Color.RED);
+        } else {
+            event_description.setBackgroundColor(Color.WHITE);
+        }
+        if(date_set == false || event_timing.getText().toString().equals(getResources().getString(R.string.select_date))){
+            ok = false;
+            Toast.makeText(this, getResources().getString(R.string.verify_event_timing), Toast.LENGTH_LONG);
+            event_timing.setBackgroundColor(Color.RED);
+        } else {
+            event_timing.setBackgroundColor(Color.WHITE);
+        }
+        return ok;
+    }
+
     public void addEvent(View v){
         // id 0 in object, id will be given after adding the object in the database.
 
-        Event e = new Event(0, city_id, event_name.getText().toString(), event_author.getText().toString(),event_description.getText().toString(), event_location.getText().toString(), event_timing.getText().toString());
-        db.addEvent(e);
-        // After adding event, go to main activity
-        Toast.makeText(this, getResources().getString(R.string.event_added_ok), Toast.LENGTH_LONG);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        if(verifyEvent(v)){
+            Event e = new Event(0, city_id, event_name.getText().toString(), event_author.getText().toString(),event_description.getText().toString(), event_location.getText().toString(), event_timing.getText().toString());
+            db.addEvent(e);
+            // After adding event, go to main activity
+            Toast.makeText(this, getResources().getString(R.string.event_added_ok), Toast.LENGTH_LONG);
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        else {
+            Toast.makeText(this, getResources().getString(R.string.event_added_fail), Toast.LENGTH_LONG);
+        }
+
     }
 
     @Override
@@ -72,5 +124,6 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
         month = month+1; // Add 1 to month to get normal month (1 for January instead of 0)
         String date = dayOfMonth+"/"+month+"/"+year;
         event_timing.setText(date);
+        date_set = true;
     }
 }
